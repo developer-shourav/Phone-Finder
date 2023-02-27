330184a90d21bfc3ef58e4481518087adaa2f896
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-27 21:32:42 +0600

    loadPhones and desplayPhones funtion added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log("Calling form App.js");
+
+
+const loadPhones = async () => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data);
+    
+}
+
+const displayPhones = (phones) => {
+    console.log(phones);
+}
+
+loadPhones();
