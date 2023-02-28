808d1dc64500031a9a3c8fa6708d15a3dc79f500
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-28 10:04:24 +0600

    dynamic components added

--- js/app.js
+++ js/app.js
@@ -9,9 +9,28 @@ const loadPhones = async() => {
 }
 
 const displayPhones = (phones) => {
-    /* console.log(phones); */
+    console.log(phones.length);
     const phonesContainer = document.getElementById("phone-container");
-    
+
+    phones.forEach( phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = ` 
+        <div class="col">
+                      <div class="card h-100">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                      </div>
+                </div>
+        `;
+
+        phonesContainer.appendChild(phoneDiv)
+
+    })
 }
 
 loadPhones();
+
